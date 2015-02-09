c216ae37f0e34bf9d74f303f779124bd605dc691
John Hsu <hsu@osrfoundation.org>
2015-02-09 14:54:43 -0800

    stylistc limit, count reverted

--- haptix.h
+++ haptix.h
@@ -86,7 +86,7 @@ struct _hxRobotInfo
   /// The number of motors is less than or equal to the number of
   /// joints. For example, one motor may control several joints through
   /// kinematic joint coupling.
-  int motor_count;
+  int count_motor;
 
   /// \brief Number of hinge joints.
   /// The joints are passive and are moved as a side effect of commanding
@@ -95,31 +95,31 @@ struct _hxRobotInfo
   ///
   /// The number of joints is greater than or equal to the number of
   /// motors.
-  int joint_count;
+  int count_joint;
 
   /// \brief Number of contact sensors.
   /// A contact sensor measures the magnitude of the force on that sensor.
-  int contact_sensor_count;
+  int count_contact_sensor
 
   /// \brief Number of IMUs (inertial measurement units).
   /// An IMU or inertial measurement unit measures the 3-dimensional linear
   /// acceleration vector and the 3-dimensional angular velocity vector
   /// experienced by the sensor.
-  int imu_count;
+  int count_imu;
 
   /// \brief Minimum and maximum motor angles (rad).
   /// An m by 2 array representing the angular limits of each motor in the
   /// robot, where m is the maximum number of motors. Each 1x2 row of the array
   /// corresponds to a motor. The first entry in the row is the lower limit
   /// of the motor. The second entry is the upper limit of the motor.
-  float motor_limit[hxMAXMOTOR][2];
+  float limit_motor[hxMAXMOTOR][2];
 
   /// \brief Minimum and maximum joint angles (rad).
   /// An n by 2 array representing the angular limits of each joint in the
   /// robot, where n is the maximum number of joints. Each 1x2 row of the array
   /// corresponds to a joint. The first entry in the row is the lower limit
   /// of the joint. The second entry is the upper limit of the joint.
-  float joint_limit[hxMAXJOINT][2];
+  float limit_joint[hxMAXJOINT][2];
 
   /// \brief Hz rate at which the robot will update.
   float update_rate;
