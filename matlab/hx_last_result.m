12bab462734921997a98b0a8007a9ff6c2dc73b8
gerkey <gerkey@osrfoundation.org>
2015-02-11 20:14:15 +0000

    hx_last_result.m edited online with Bitbucket

--- matlab/hx_last_result.m
+++ matlab/hx_last_result.m
@@ -5,6 +5,6 @@
 % HX_CONNECT should have been called first.
 %
 % Return values:
-%   result (string): Description of the result of that last hx_*() call.
+%   result (string): Description of the result of the last hx_*() call.
 %
 % Throws an error if something failed.
