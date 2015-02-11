5c614475dc946e416dd6de956a109c181373f2d1
gerkey <gerkey@osrfoundation.org>
2015-02-11 20:15:59 +0000

    hx_read_sensors.m edited online with Bitbucket

--- matlab/hx_read_sensors.m
+++ matlab/hx_read_sensors.m
@@ -1,9 +1,10 @@
 %HX_READ_SENSORS Read sensor data from simulator/robot.
 %
 % Similar to HX_UPDATE, this function reads the latest sensor information
-% from the simulator/robot in a non-blocking manner. This function may be
-% called as often as desired, but the robot is only guaranteed to update at
-% the rate returned from HX_ROBOT_INFO.
+% from the simulator/robot in a non-blocking manner. Use this function when
+% you want to read the sensor data without sending a new command.  This
+% function may be called as often as desired, but the robot is only
+% guaranteed to update at the rate returned from HX_ROBOT_INFO.
 %
 % HX_CONNECT should have been called first.
 %
