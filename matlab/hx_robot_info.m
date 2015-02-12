8c256a106826e8ced9069a9a9a95851904129586
Brian Gerkey <gerkey@osrfoundation.org>
2015-02-12 12:15:00 -0800

    Specified return value shapes

--- matlab/hx_robot_info.m
+++ matlab/hx_robot_info.m
@@ -17,13 +17,13 @@
 %       measurement unit measures the 3-dimensional linear acceleration
 %       vector and the 3-dimensional angular velocity vector experienced by
 %       the sensor.
-%     motor_limit (float array) : An N by 2 (or 2 by N) array representing
+%     motor_limit (float array) : An N by 2 array representing
 %       the angular limits of each motor in the robot, where N is the maximum
 %       number of motors (rad). Each 1x2 row/column of the array corresponds to
 %       a motor. The first entry in the row/column is the lower limit of the motor.
 %       The second entry is the upper limit of the motor.
 %     joint_limit (float array) : Minimum and maximum joint angles (rad).  An M by 2
-%       (or 2 by M) array representing the angular limits of each joint in the robot,
+%       array representing the angular limits of each joint in the robot,
 %       where M is the maximum number of joints. Each 1x2 row/column of the array
 %       corresponds to a joint. The first entry in the row/column is the lower
 %       limit of the joint. The second entry is the upper limit of the joint.
--- matlab/hx_update.m
+++ matlab/hx_update.m
@@ -32,23 +32,23 @@
 % Return values:
 %   sensor: Structure with the following named fields:
 %     time_stamp (float) : Time at which the sensor reading was taken (s).
-%     motor_pos (float array) : An N by 1 (or 1 by N) array of motor positions (rad).
-%     motor_vel (float array) : An N by 1 (or 1 by N) array of motor velocities (rad/s).
-%     motor_torque (float array) : An N by 1 (or 1 by N) array of motor torques (Nm).
-%     joint_pos (float array) : An M by 1 (or 1 by M) array of joint positions (rad).
-%     joint_vel (float array) : An M by 1 (or 1 by M) array of joint velocities (rad/s).
-%     contact (float array) : An L by 1 (or 1 by L) array of contact force magnitudes (N).
-%     imu_linear_acc (float array) : A K by 3 (or 3 by K) array of IMU accelerometer
+%     motor_pos (float array) : An N by 1  array of motor positions (rad).
+%     motor_vel (float array) : An N by 1  array of motor velocities (rad/s).
+%     motor_torque (float array) : An N by 1  array of motor torques (Nm).
+%     joint_pos (float array) : An M by 1  array of joint positions (rad).
+%     joint_vel (float array) : An M by 1  array of joint velocities (rad/s).
+%     contact (float array) : An L by 1  array of contact force magnitudes (N).
+%     imu_linear_acc (float array) : A K by 3  array of IMU accelerometer
 %       data (m/s^2).  Each row/column is a 3-dimensional vector of accelerometer
 %       output, which comprises the vector difference (a-g), where a is the
 %       linear acceleration and g is the gravity vector.  This measurement
 %       is expressed in a body-fixed frame.  The entries of each row/column are
 %       measured in meters per second squared and ordered (x, y, z).
-%     imu_angular_vel (float array) : A K by 3 (or 3 by K) array of IMU gyro data (rad/s).
+%     imu_angular_vel (float array) : A K by 3 array of IMU gyro data (rad/s).
 %       Each row/column is a 3-dimensional angular velocity vector.  This measurement
 %       is expressed in a body-fixed frame.  The entries of each row/column are
 %       measured in radians per second and ordered (x, y, z).
-%     imu_orientation (float array) : A K by 4 (or 4 by K) array of IMU orientation data
+%     imu_orientation (float array) : A K by 4 array of IMU orientation data
 %       (quaternion).  Each row/column provides a 4-element quaternion representation
 %       of the estimated orientation of the corresponding IMU.  A given
 %       simulator/robot might not support this functionality.
